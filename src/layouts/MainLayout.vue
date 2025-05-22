d7ff043fa8b071c131e245e3b66bf1c0f9a3e322
pedro <pedro.gregorio@paganini.tech>
2025-05-22 18:15:38 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-10 text-white q-pa-md">
+      <div class="row justify-between items-center">
+        <div>&copy; 2025 SuaMarca. Todos os direitos reservados.</div>
+        <div class="row q-gutter-sm">
+          <q-btn dense flat icon="mdi-instagram" />
+          <q-btn dense flat icon="mdi-whatsapp" />
+        </div>
+      </div>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-none">
+    <div class="bg-grey full-width" style="height: 70vh">
+      <div class="flex justify-center text-center">carrossel</div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+// Sem lógica por enquanto, só visual.
+</script>
+
+<style scoped></style>
